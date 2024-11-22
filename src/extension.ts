005e0d1f26e4499b7f7fba80568359de4fc2ceec
hatoo <hato2000@gmail.com>
2024-11-22 14:12:49 +0900

    a

--- src/extension.ts
+++ src/extension.ts
@@ -14,14 +14,14 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('shadertoy-sound.helloWorld', async () => {
+	const disposable = vscode.commands.registerCommand('shadertoy-sound.preview', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from shadertoy-sound!');
 
 		if (currentPanel === undefined) {
 			currentPanel = vscode.window.createWebviewPanel('preview',
-				'preview',
+				'shadertoy sound preview',
 				vscode.ViewColumn.Two,
 				{
 					enableScripts: true,
