68e3700262b7373cdc75cc7b09d855507c225fde
hatoo <hato2000@gmail.com>
2024-11-22 20:16:01 +0900

    slider

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,16 @@
 import * as THREE from 'three';
-import React, { useEffect, useState } from "react";
-import { Stack, Checkbox, Slider, FormControlLabel } from '@mui/material';
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Stack, Checkbox, Slider, FormControlLabel, styled } from '@mui/material';
 import { VolumeDown, VolumeUp } from '@mui/icons-material';
+
+const CustomSlider = styled(Slider)(({ theme }) => ({
+    "& .MuiSlider-thumb": {
+        transition: 'none'
+    },
+    "& .MuiSlider-track": {
+        transition: 'none'
+    },
+}));
 
 const fragmentShaderFooter = `
 
@@ -30,6 +39,7 @@ const renderer = new THREE.WebGLRenderer();
 const audioCtx = new window.AudioContext();
 const audioBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * DURATION, audioCtx.sampleRate);
 const gain = audioCtx.createGain();
+gain.connect(audioCtx.destination);
 
 export default function App() {
     const [error, setError] = useState('');
@@ -37,16 +47,41 @@ export default function App() {
     const [gainValue, setGainValue] = useState(gain.gain.value);
     const [loaded, setLoaded] = useState(false);
     const [autoPlay, setAutoPlay] = useState(false);
+    const [start, setStart] = useState(0);
+    const [current, setCurrent] = useState(start);
+    const [end, setEnd] = useState(DURATION);
+    const [lastTimeStamp, setLastTimeStamp] = useState(audioCtx.currentTime);
+
+    const [currentNode, setCurrentNode] = useState<AudioBufferSourceNode | null>(null);
 
     const play = () => {
         const audioBufferSourceNode = audioCtx.createBufferSource();
         audioBufferSourceNode.buffer = audioBuffer;
         audioBufferSourceNode.connect(gain);
-        audioBufferSourceNode.start(0);
+        setLastTimeStamp(audioCtx.currentTime);
+        audioBufferSourceNode.start(0, start, end - start);
         audioCtx.resume();
+        setCurrentNode(audioBufferSourceNode);
     };
 
-    gain.connect(audioCtx.destination)
+    const requestId = useRef<ReturnType<typeof requestAnimationFrame>>();
+
+    const animate = () => {
+        console.log('animate');
+        if (currentNode) {
+            setCurrent(start + currentNode.context.currentTime - lastTimeStamp);
+        }
+        requestId.current = requestAnimationFrame(animate);
+    };
+
+    useEffect(() => {
+        requestId.current = requestAnimationFrame(animate);
+        return () => {
+            if (requestId.current) {
+                cancelAnimationFrame(requestId.current);
+            }
+        };
+    }, [currentNode]);
 
     useEffect(() => {
         gain.gain.value = gainValue;
@@ -151,6 +186,27 @@ export default function App() {
             }} />
             <VolumeUp />
         </Stack>
+        <Slider
+            min={0}
+            max={DURATION}
+            step={0.1}
+            valueLabelDisplay="on"
+            value={[start, end]}
+            onChange={(e, value) => {
+                const [a, b] = value as [number, number];
+
+                setStart(Math.min(a, b));
+                setEnd(Math.max(a, b));
+            }}
+        />
+        <CustomSlider
+            min={start}
+            max={end}
+            step={0.01}
+            valueLabelDisplay="on"
+            valueLabelFormat={(value) => value.toFixed(2)}
+            value={current}
+        />
         <button onClick={() => {
             play();
         }}>Play</button>
