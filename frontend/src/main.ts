cabbe8b931ae077a4a0bf260a8130d4a210349fc
hatoo <hato2000@gmail.com>
2024-11-22 14:50:48 +0900

    show error

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -15,8 +15,21 @@ const fragmentShaderFooter = `
     }
 `;
 
+function setError(message: string) {
+    document.getElementById('error')!.textContent = message;
+}
 
 const renderer = new THREE.WebGLRenderer();
+renderer.debug.onShaderError = (
+    gl: WebGLRenderingContext,
+    program: WebGLProgram,
+    glVertexShader: WebGLShader,
+    glFragmentShader: WebGLShader,
+) => {
+    let info = gl.getShaderInfoLog(glFragmentShader)?.trim() ?? '';
+    console.log(info);
+    setError(info);
+};
 
 const DURATION = 180 // 再生秒数
 
@@ -29,61 +42,11 @@ const audioBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * DURATION, aud
 const samples = WIDTH * HEIGHT
 const numBlocks = (audioCtx.sampleRate * DURATION) / samples
 
-//
-
-/*
-const scene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
-camera.position.set(0, 0, 1);
-camera.lookAt(scene.position);
-
-const uniforms = {
-    iSampleRate: {
-        type: 'f',
-        value: audioCtx.sampleRate
-    },
-    blockOffset: {
-        type: 'f',
-        value: 0
-    },
-};
-
-const material = new THREE.ShaderMaterial({
-    uniforms: uniforms,
-    fragmentShader: document.getElementById('fragmentShader')?.textContent + fragmentShaderFooter // fragmentShader,
-});
-const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
-scene.add(mesh);
-
-const target = new THREE.WebGLRenderTarget(WIDTH, HEIGHT);
-
-const renderCtx = renderer.getContext()
-
-for (let i = 0; i < numBlocks; i++) {
-    uniforms.blockOffset.value = i * samples / audioCtx.sampleRate // シェーダーに数値の割り当て
-    renderer.setRenderTarget(target) // オフスクリーンレンダリング 
-    renderer.render(scene, camera)
-
-    const pixels = new Uint8Array(WIDTH * HEIGHT * 4)
-    renderCtx.readPixels(0, 0, WIDTH, HEIGHT, renderCtx.RGBA, renderCtx.UNSIGNED_BYTE, pixels)  // 描画結果を取得
-
-    const outputDataL = audioBuffer.getChannelData(0) // 音声左チャンネル割り当て
-    const outputDataR = audioBuffer.getChannelData(1) // 音声右チャンネル割り当て
-    for (let j = 0; j < samples; j++) {
-        outputDataL[i * samples + j] = (pixels[j * 4 + 0] + 256 * pixels[j * 4 + 1]) / 65535 * 2 - 1
-        outputDataR[i * samples + j] = (pixels[j * 4 + 2] + 256 * pixels[j * 4 + 3]) / 65535 * 2 - 1
-    }
-}
-
-audioBufferSourceNode.buffer = audioBuffer // 音を割り当てて
-audioBufferSourceNode.connect(audioCtx.destination) // 出力先を指定
-audioBufferSourceNode.start(0) // 再生
-*/
-
 window.addEventListener('message', event => {
     const message = event.data; // The JSON data our extension sent
     switch (message.command) {
         case 'setShader':
+            setError('');
             const scene = new THREE.Scene();
             const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
             camera.position.set(0, 0, 1);
@@ -102,7 +65,7 @@ window.addEventListener('message', event => {
 
             const material = new THREE.ShaderMaterial({
                 uniforms: uniforms,
-                fragmentShader: message.shader + fragmentShaderFooter // fragmentShader,
+                fragmentShader: '#line 1 1\n' + message.shader + fragmentShaderFooter // fragmentShader,
             });
             const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), material);
             scene.add(mesh);
--- src/extension.ts
+++ src/extension.ts
@@ -45,6 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 <body>
 	<h1>HELLO</h1>
+	<pre id="error"></pre>
     <script type="module" src="${mainPath}"></script>
 </body>
 </html>`;
