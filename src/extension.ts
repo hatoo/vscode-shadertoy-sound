a3bdf0a1627ea80fa1e0ffc61684a6b59edf278d
hatoo <hato2000@gmail.com>
2024-11-23 15:48:21 +0900

    Add dispose command

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('shadertoy-sound.preview', async () => {
+	const preview = vscode.commands.registerCommand('shadertoy-sound.preview', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from shadertoy-sound!');
@@ -68,7 +68,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 	});
 
-	context.subscriptions.push(disposable);
+	const disposeWebview = vscode.commands.registerCommand('shadertoy-sound.dispose', async () => {
+		if (currentPanel !== undefined) {
+			currentPanel.dispose();
+			currentPanel = undefined;
+		}
+	});
+
+	context.subscriptions.push(preview);
 }
 
 // This method is called when your extension is deactivated
